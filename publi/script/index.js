17736358661ca4da57883e33aba0f92ed61a988e
nikodacat <82635097+edison1119@users.noreply.github.com>
2025-04-30 02:39:49 +0800

    setting up chatroom

--- publi/script/index.js
+++ publi/script/index.js
@@ -23,7 +23,12 @@ window.addEventListener('load', function () {
         //firebase.auth.PhoneAuthProvider.PROVIDER_ID
       ],
     };
-  
+    if(this.localStorage.getItem('loginsuccess')){
+      document.getElementById("loginalert").classList.remove("d-none");
+      setTimeout(() => {
+          document.getElementById("loginalert").classList.add("d-none");
+      }, 3000);
+    }
     firebase.auth().onAuthStateChanged(function (user) {
       if (user) {
         // User is signed in, so display the "sign out" button and login info.
@@ -31,7 +36,8 @@ window.addEventListener('load', function () {
         document.getElementById('loginbtn').onclick = function () {
           firebase.auth().signOut();
         };
-        document.getElementById('login-info').innerHTML = `user:${user.displayName}`;
+        console.log("yesyesyes");
+        document.getElementById('login-info').innerHTML = `user:${user.email}`;
         console.log(`Signed in as ${user.displayName} (${user.email})`);
         user.getIdToken().then(function (token) {
           // Add the token to the browser's cookies. The server will then be
--- publi/script/login.js
+++ publi/script/login.js
@@ -1,11 +1,12 @@
 //AUTH ARE COPIED FROM firebase documents
-
+//TESTING
 const auth = firebase.auth();
 function login(){
     console.log("yes");
-    var usernamehtml = document.getElementById("username");
+    var emailhtml = document.getElementById("email");
     var passwordhtml = document.getElementById("password");
-    var fireauth= firebase.auth();
+    var email = emailhtml.value;
+    var password =  passwordhtml.value;
     firebase.auth().signInWithEmailAndPassword(email, password)
     .then((userCredential) => {
         // Signed in
@@ -17,24 +18,44 @@ function login(){
     .catch((error) => {
         var errorCode = error.code;
         var errorMessage = error.message;
-        console.log(errorMessage)
+        console.log(errorMessage);
+        document.getElementById("login-alert").classList.remove("d-none");
+        setTimeout(() => {
+            document.getElementById("login-alert").classList.add("d-none");
+        }, 3000);
     });
 
 }
 function createuser(){
-
+    var emailhtml = document.getElementById("email");
+    var passwordhtml = document.getElementById("password");
+    var email = emailhtml.value;
+    var password =  passwordhtml.value;
     auth.createUserWithEmailAndPassword(email, password)
     .then((userCredential) => {
-        // Signed in 
         var user = userCredential.user;
-        
-        // ...
+        window.alert("account created, automatically logging you in");
+        firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // Signed in
+            var user = userCredential.user;
+            // ...
+            localStorage.setItem('loginSuccess', 'true');
+            window.location.href = 'index.html';
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorMessage);
+            window.alert("auto login failed...");
+            
+        });
     })
     .catch((error) => {
         var errorCode = error.code;
         var errorMessage = error.message;
         console.log(errorMessage);
-
-    });
+        window.alert("account creation failed")
+    }); 
 }
 console.log("loaded");
